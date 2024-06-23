011e686bc1edb4c8ae76a961c1e77010d6e62ec4
IceHacy <78807447+IceHacy@users.noreply.github.com>
2024-06-23 15:53:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const musicDir = 'assets/music/';
-    fetch(musicDir)
-        .then(response => response.text())
-        .then(text => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(text, 'text/html');
-            const files = [...doc.querySelectorAll('a')]
-                .map(link => link.getAttribute('href'))
-                .filter(href => href.endsWith('.mp3'));
-            if (files.length > 0) {
-                const randomIndex = Math.floor(Math.random() * files.length);
-                const randomFile = files[randomIndex];
-                const audioPlayer = document.getElementById('audioPlayer');
-                audioPlayer.src = musicDir + randomFile;
-                audioPlayer.play();
-            } else {
-                console.error('No music files found.');
-            }
-        })
-        .catch(error => console.error('Error fetching music files:', error));
+    const musicdir = [
+        'assets/music/Waterworld - Map.mp3',
+        'assets/music/Waterworld - Shop.mp3'
+    ];
+    const randomIndex = Math.floor(Math.random() * musicdir.length);
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.src = musicdir[randomIndex];
+    audioPlayer.play();
 });
